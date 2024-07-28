e5ab80923d3330b3aa0cd07f195e5f97369a964b
parykhan-jameel <parexan.00443431@gmail.com>
2024-07-28 23:18:24 +0300

    added a single meal page and designed it

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,11 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        'custom-gradient': 'linear-gradient(to bottom, #013243, #B0E0E6)',
+      },
+    },
   },
   plugins: [],
 }
